53b75d5634bb543c2463fd1b2d2046d3db0db760
SalDev-lab <scrupulousscr@gmail.com>
2025-09-26 22:13:34 -0700

    Add files via upload

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+
+const Navbar = () => {
+  return (
+    <div className='admin-navbar'>
+      <div className="navbar-left">
+        <img className='logo' src={assets.logo} alt="Logo" />
+        <h2 className="admin-title">Admin Panel</h2>
+      </div>
+      <div className="navbar-right">
+        <div className="profile-section">
+          <img className='profile' src={assets.profile_image} alt="Profile" />
+          <p className='profile-name'>Admin</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
